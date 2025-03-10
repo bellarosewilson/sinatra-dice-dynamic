e3c39d32669a6a31bc6b24ac97cf751c9cbf2fb6
Bella Rose Wilson <bwilson9893@gmail.com>
2025-03-10 16:24:47 +0000

    updated

--- app.rb
+++ app.rb
@@ -5,50 +5,11 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dice/2/6") do
-  @rolls = []
+# Dynamic route for any dice/sides
+get("/dice/:dice/:sides") do
+  @num_dice = params[:dice].to_i
+  @num_sides = params[:sides].to_i
+  @rolls = Array.new(@num_dice) { rand(1..@num_sides) }
 
-  2.times do
-    die = rand(1..6)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_six)
-end
-
-get("/dice/2/10") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..10)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_ten)
-end
-
-get("/dice/1/20") do
-  @rolls = []
-
-  1.times do
-    die = rand(1..20)
-
-    @rolls.push(die)
-  end
-
-  erb(:one_twenty)
-end
-
-get("/dice/5/4") do
-  @rolls = []
-
-  5.times do
-    die = rand(1..4)
-
-    @rolls.push(die)
-  end
-
-  erb(:five_four)
+  erb(:roll)  # adding `views/roll.erb`
 end
